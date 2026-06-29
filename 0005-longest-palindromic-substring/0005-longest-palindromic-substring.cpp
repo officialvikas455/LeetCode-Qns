2592510adab74f6618313d17fd600cce2004ba31
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-06-29 22:25:31 +0530

    Time: 856 ms (7.69%), Space: 9.4 MB (62.48%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool solve(string &s, int i, int j) {
+        if (i >= j) return true;
+        if (s[i] == s[j]) return solve(s, i + 1, j - 1);
+        return false;
+    }
+
+    string longestPalindrome(string s) {
+        int n = s.length();
+        int maxlen = 0; 
+        int sp = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                if (solve(s, i, j)) {
+                    if (j - i + 1 > maxlen) {
+                        maxlen = j - i + 1; 
+                        sp = i;
+                    }
+                }
+            }
+        }
+        return s.substr(sp, maxlen);
+    }
+};
