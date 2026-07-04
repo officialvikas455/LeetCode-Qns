f0fc4c403bfdc71aef62788adb7f94304695833c
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-04 21:47:58 +0530

    Time: 0 ms (100%), Space: 14.9 MB (85.77%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -11,25 +11,16 @@
  */
 class Solution {
 public:
+    void rec(TreeNode* root, int level, vector<int> &ans){
+        if(root==NULL) return;
+        if(ans.size() == level) ans.push_back(root->val);
+
+        rec(root->right, level+1,ans);
+        rec(root->left, level+1, ans);
+    }
     vector<int> rightSideView(TreeNode* root) {
-        vector<int> result;
-        if (!root) return result;
-        queue<TreeNode*> q;
-        q.push(root);
-        while (!q.empty()) {
-            int size = q.size();
-            // process one level
-            for (int i = 0; i < size; ++i) {
-                TreeNode* node = q.front();
-                q.pop();
-                // if its the last node at this level
-                // record it
-                if (i == size - 1) result.push_back(node->val);
-                // now add children to queue
-                if (node->left) q.push(node->left);
-                if (node->right) q.push(node->right);
-            }
-        }
-        return result;
+        vector<int> ans;
+        rec(root,0,ans);
+        return ans;
     }
 };
