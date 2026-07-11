8a60200fb50e32c775707194b0f4169eca78355b
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-11 12:58:20 +0530

    Time: 3 ms (63.89%), Space: 13.3 MB (31.39%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+void recursion(vector<int> nums, int i, int j, vector<vector<int> > &res) {
+        if (i == j-1) {
+            res.push_back(nums);
+            return;
+        }
+        for (int k = i; k < j; k++) {
+            if (i != k && nums[i] == nums[k]) continue;
+            swap(nums[i], nums[k]);
+            recursion(nums, i+1, j, res);
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+         sort(nums.begin(), nums.end());
+        vector<vector<int> >res;
+        recursion(nums, 0, nums.size(), res);
+        return res;
+    }
+};
