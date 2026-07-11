9106ac088d0a85d9251d3bb4b07a57f373799ddd
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-11 13:16:09 +0530

    Time: 7 ms (18.97%), Space: 9.6 MB (76.17%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,26 +1,11 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length() != t.length()){
-            return false;
-        }
+        if(s.size() != t.size()) return 0;
+        sort(s.begin(), s.end());
+        sort(t.begin(), t.end());
+
+        return s == t;
         
-        int count[26] = {0};
-        
-        // Count frequency from s
-        for(int i = 0; i < s.length(); i++){
-            count[s[i] - 'a']++;
-        }
-        
-        // Decrease frequency using t
-        for(int i = 0; i < t.length(); i++){
-            int idx = t[i] - 'a';
-            count[idx]--;
-            if(count[idx] < 0){   // More chars in t than in s
-                return false;
-            }
-        }
-        
-        return true;  // All matched
     }
 };
