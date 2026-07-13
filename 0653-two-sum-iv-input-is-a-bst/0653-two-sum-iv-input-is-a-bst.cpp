b39f44a7b106a2e126cbf45e45c8b5cde19678b9
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-13 11:54:05 +0530

    Time: 3 ms (52.24%), Space: 37.1 MB (95.04%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.cpp
@@ -1,25 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// /**
+//  * Definition for a binary tree node.
+//  * struct TreeNode {
+//  *     int val;
+//  *     TreeNode *left;
+//  *     TreeNode *right;
+//  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+//  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+//  * };
+//  */
+// /**
+
 class Solution {
 public:
     void inorder(TreeNode* root, vector<int> &ans){
@@ -34,13 +25,14 @@ public:
         inorder(root, ans);   
 
         int n = ans.size();
+        int i = 0;
+        int j = n-1;
         
-        for(int i = 0; i < n; i++){
-            for(int j = i + 1; j < n; j++){
-                if(ans[i] + ans[j] == k){
-                    return true;
-                }
-            }
+        while(i < j){
+            int sum = ans[i] + ans[j];
+            if(sum == k) return true;
+            if( sum < k) i++;
+            else j--;
         }
         return false;
     }
