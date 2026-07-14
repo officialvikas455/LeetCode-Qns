66dcc086c922cab3cf88c6dd3f737a99e6ddc020
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-14 13:46:32 +0530

    Time: 0 ms (100%), Space: 10.7 MB (46.31%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+   int solve(vector<int>& nums, int i , vector<int> &dp){
+    if(i<0) return 0;
+    if(i==0) return nums[0];
+
+    // if value is already computed
+    if(dp[i] != -1)
+        return dp[i];
+
+    int pick = nums[i] + solve(nums, i-2, dp);
+    int notpick = solve(nums, i-1, dp);
+
+    return dp[i] = max(pick, notpick);
+
+
+   }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        return solve(nums, n-1, dp);
+        
+    }
+};
