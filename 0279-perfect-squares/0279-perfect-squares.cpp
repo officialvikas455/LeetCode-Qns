d8c4c90383b835a4422852a9ff591d34e88e5020
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-15 20:00:47 +0530

    Time: 38 ms (85.67%), Space: 13.2 MB (56.18%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,21 +1,40 @@
+// class Solution {
+// public:
+   // memoization
+//    int solve(int n, vector<int>&dp){
+//     if(n==0) return 0;
+
+//     if(dp[n] != -1) return dp[n];
+
+//     int minCount = INT_MAX;
+
+//     for(int i=1; i*i<=n; i++){
+//         int result = 1 + solve(n-i*i,dp);
+//         minCount = min(minCount, result);
+//     }
+//     return dp[n] = minCount;
+//    }
+//     int numSquares(int n) {
+//         vector<int> dp(n+1,-1);
+//         return solve(n,dp);
+//     }
+// };
+
+//Tabulation 
+
+
 class Solution {
 public:
-
-   int solve(int n, vector<int>&dp){
-    if(n==0) return 0;
-
-    if(dp[n] != -1) return dp[n];
-
-    int minCount = INT_MAX;
-
-    for(int i=1; i*i<=n; i++){
-        int result = 1 + solve(n-i*i,dp);
-        minCount = min(minCount, result);
-    }
-    return dp[n] = minCount;
-   }
     int numSquares(int n) {
-        vector<int> dp(n+1,-1);
-        return solve(n,dp);
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                int square = j * j;
+                dp[i] = min(dp[i], 1 + dp[i - square]);
+            }
+        }
+        return dp[n];
     }
 };
