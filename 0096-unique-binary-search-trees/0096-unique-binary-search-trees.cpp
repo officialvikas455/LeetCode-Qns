c61cc47f8fc1b881f174b83ab09b5a230237323a
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-16 15:50:14 +0530

    Time: 0 ms (100%), Space: 7.9 MB (79.47%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,19 +1,32 @@
+
+//  int solveMem(int n, vector<int> &dp){
+//     if(n <= 1) return 1;
+
+//     if(dp[n] != -1) return dp[n];
+    
+//      int ans = 0;
+
+//      for(int i=1; i<=n; i++){
+//         ans += solveMem(i-1,dp) * solveMem(n-i,dp);
+//      }
+//      return dp[n] = ans;
+//  }
+
 class Solution {
 public:
- int solveMem(int n, vector<int> &dp){
-    if(n <= 1) return 1;
+    int solveTab(int n, vector<int>& dp) {
+        dp[0] = dp[1] = 1; 
+        
+        for (int i = 2; i <= n; i++) { 
+            for (int j = 0; j < i; j++) {
+                dp[i] += dp[j] * dp[i - j - 1];
+            }
+        }
+        return dp[n];
+    }
 
-    if(dp[n] != -1) return dp[n];
-    
-     int ans = 0;
-
-     for(int i=1; i<=n; i++){
-        ans += solveMem(i-1,dp) * solveMem(n-i,dp);
-     }
-     return dp[n] = ans;
- }
     int numTrees(int n) {
-        vector<int> dp(n+1,-1);
-        return solveMem(n, dp);
+        vector<int> dp(n + 1, 0);
+        return solveTab(n, dp);
     }
 };
