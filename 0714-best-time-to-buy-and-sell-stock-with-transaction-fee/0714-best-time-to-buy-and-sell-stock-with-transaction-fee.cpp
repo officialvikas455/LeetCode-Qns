0ff80876eba8ff3fed89cc9060f799fb744f000b
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-20 21:17:10 +0530

    Time: 108 ms (43.94%), Space: 88 MB (62.12%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -22,20 +22,24 @@ public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
     //   vector<vector<int>> dp(n+1, vector<int>(2,-1));
-       vector<vector<int>> dp(n+1, vector<int>(2,0));
+    //    vector<vector<int>> dp(n+1, vector<int>(2,0));
+       vector<int> prev(2,0);
+     
 
 
        for(int ind = n-1; ind >= 0; ind--){
+           vector<int> curr(2,0);
         for(int buy=0; buy<=1; buy++){
 
             if(buy){
-                 dp[ind][buy] = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+                 curr[buy] = max(-prices[ind]+ prev[0], prev[1]);
             }else{
-                 dp[ind][buy] = max(prices[ind]-fee+dp[ind+1][1],dp[ind+1][0]);
+                 curr[buy] = max(prices[ind]-fee+prev[1], prev[0]);
             }
          }
+         prev = curr;
        }
         // return solve(0, 1, n, fee, prices,dp);
-        return dp[0][1];
+        return prev[1];
     }
 };
