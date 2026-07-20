385132cc52b6fbdb2efadeb19d22ea9f3c399c39
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-20 20:54:13 +0530

    Time: 123 ms (27.68%), Space: 108.6 MB (8.43%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,32 +1,27 @@
-#include <vector>
-#include <cmath>
-#include <cstring>
+
 using namespace std;
 
 class Solution {
-    int dp[50005][2];
 
-    int solve(int ind, int buy, int n, int fee, vector<int>& prices) {
+    int solve(int ind, int buy, int n, int fee, vector<int>& prices,vector<vector<int>> &dp) {
         if (ind == n) return 0;
         if (dp[ind][buy] != -1) return dp[ind][buy];
 
-        int profit = 0;
         if (buy) {
             // We can either buy or skip buying
-            profit = max(-prices[ind] + solve(ind + 1, 0, n, fee, prices), 
-                         solve(ind + 1, 1, n, fee, prices));
+            return dp[ind][buy] = max(-prices[ind] + solve(ind + 1, 0, n, fee, prices,dp), 
+                         solve(ind + 1, 1, n, fee, prices,dp));
         } else {
             // We can either sell (and pay the fee) or skip selling
-            profit = max(prices[ind] - fee + solve(ind + 1, 1, n, fee, prices), 
-                         solve(ind + 1, 0, n, fee, prices));
+           return  dp[ind][buy]  = max(prices[ind] - fee + solve(ind + 1, 1, n, fee, prices,dp), 
+                         solve(ind + 1, 0, n, fee, prices,dp));
         }
-        return dp[ind][buy] = profit;
     }
 
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
-        memset(dp, -1, sizeof(dp)); 
-        return solve(0, 1, n, fee, prices);
+      vector<vector<int>> dp(n+1, vector<int>(2,-1));
+        return solve(0, 1, n, fee, prices,dp);
     }
 };
