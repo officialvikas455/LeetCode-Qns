4789173c461ca199d978f0f6f67cc6d61464d44c
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-21 11:55:28 +0530

    Time: 2 ms (39.11%), Space: 16.1 MB (53.06%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,25 +1,56 @@
+// class Solution {
+// public:
+
+
+//     int solve(int ind, int buy, int n, vector<int>& prices,vector<vector<int>> &dp) {
+//         if (ind >= n) return 0;
+//         if (dp[ind][buy] != -1) return dp[ind][buy];
+
+//         if (buy) {
+//             // We can either buy or skip buying
+//             return dp[ind][buy] = max(-prices[ind] + solve(ind + 1, 0, n, prices,dp), 
+//                          solve(ind + 1, 1, n, prices,dp));
+//         } else {
+//             //  If we sell, we jump to ind + 2 to enforce a 1-day cooldown
+//            return  dp[ind][buy]  = max(prices[ind]  + solve(ind + 2, 1, n, prices,dp), 
+//                          solve(ind + 1, 0, n, prices,dp));
+//         }
+//     }
+
+//     int maxProfit(vector<int>& prices) {
+//         int n = prices.size();
+//         vector<vector<int>> dp(n, vector<int>(2,-1));
+//         return solve(0,1,n,prices,dp);
+//     }
+// };
+
+
 class Solution {
 public:
-
-
-    int solve(int ind, int buy, int n, vector<int>& prices,vector<vector<int>> &dp) {
-        if (ind >= n) return 0;
-        if (dp[ind][buy] != -1) return dp[ind][buy];
-
-        if (buy) {
-            // We can either buy or skip buying
-            return dp[ind][buy] = max(-prices[ind] + solve(ind + 1, 0, n, prices,dp), 
-                         solve(ind + 1, 1, n, prices,dp));
-        } else {
-            //  If we sell, we jump to ind + 2 to enforce a 1-day cooldown
-           return  dp[ind][buy]  = max(prices[ind]  + solve(ind + 2, 1, n, prices,dp), 
-                         solve(ind + 1, 0, n, prices,dp));
-        }
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n, vector<int>(2,-1));
-        return solve(0,1,n,prices,dp);
+        if (n == 0) return 0;
+
+        // DP table size (n+2) x 2 initialized to 0. 
+        vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+
+        for (int ind = n - 1; ind >= 0; ind--) {
+            for (int buy = 0; buy <= 1; buy++) {
+                if (buy == 1) {
+                    // Option 1: Buy today -> next day we must sell (buy=0)
+                    // Option 2: Skip today -> next day we can still buy (buy=1)
+                    dp[ind][buy] = max(-prices[ind] + dp[ind + 1][0], 
+                                       dp[ind + 1][1]);
+                } else {
+                    // Option 1: Sell today -> skip next day due to cooldown (jump to ind+2, buy=1)
+                    // Option 2: Skip today -> next day we must still sell (buy=0)
+                    dp[ind][buy] = max(prices[ind] + dp[ind + 2][1], 
+                                       dp[ind + 1][0]);
+                }
+            }
+        }
+        return dp[0][1];
     }
 };
+
+
