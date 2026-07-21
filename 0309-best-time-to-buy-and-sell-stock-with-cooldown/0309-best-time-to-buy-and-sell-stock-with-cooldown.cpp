c06f33c4df9fcd01442ba30835fc231e020bafff
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-21 12:02:02 +0530

    Time: 2 ms (39.11%), Space: 16.5 MB (9.34%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -33,23 +33,27 @@ public:
 
         // DP table size (n+2) x 2 initialized to 0. 
         vector<vector<int>> dp(n + 2, vector<int>(2, 0));
+        vector<int> prev1(2, 0), prev2 = prev1;
 
         for (int ind = n - 1; ind >= 0; ind--) {
+            vector<int> curr(2, 0);
             for (int buy = 0; buy <= 1; buy++) {
                 if (buy == 1) {
                     // Option 1: Buy today -> next day we must sell (buy=0)
                     // Option 2: Skip today -> next day we can still buy (buy=1)
-                    dp[ind][buy] = max(-prices[ind] + dp[ind + 1][0], 
-                                       dp[ind + 1][1]);
+                   curr[buy] = max(-prices[ind] + prev1[0], 
+                                       prev1[1]);
                 } else {
                     // Option 1: Sell today -> skip next day due to cooldown (jump to ind+2, buy=1)
                     // Option 2: Skip today -> next day we must still sell (buy=0)
-                    dp[ind][buy] = max(prices[ind] + dp[ind + 2][1], 
-                                       dp[ind + 1][0]);
+                   curr[buy] = max(prices[ind] + prev2[1], 
+                                       prev1[0]);
                 }
             }
+            prev2 = prev1;
+            prev1 = curr;
         }
-        return dp[0][1];
+        return prev1[1];
     }
 };
 
