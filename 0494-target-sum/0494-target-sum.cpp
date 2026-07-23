7670d3812b09243ba9c797ec6356a05e6d1cb48b
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-23 14:09:04 +0530

    Time: 0 ms (100%), Space: 12.4 MB (68%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        
         int total = 0;
-        for(int x : nums) total += x;
-
-        if((target + total) % 2 != 0 || abs(target) > total) return 0;
-
+        for (int x : nums) total += x;
+    
+        if ((target + total) % 2 != 0 || abs(target) > total) return 0;
+        
         int sum = (target + total) / 2;
-
-        vector<vector<int>> dp(n+1, vector<int>(sum+1, 0));
-
-        dp[0][0] = 1; 
-
-        for(int i=1; i<=n; i++){
-            for(int j=0; j<=sum; j++){
-                if(nums[i-1] <= j){
-                    dp[i][j] = dp[i-1][j-nums[i-1]] + dp[i-1][j];
-                }else{
-                    dp[i][j] = dp[i-1][j];
-                }
+        vector<int> dp(sum + 1, 0);
+        dp[0] = 1; 
+        
+        for (int x : nums) {
+            for (int j = sum; j >= x; j--) {
+                dp[j] += dp[j - x];
             }
         }
-        return dp[n][sum];
+        
+        return dp[sum];
     }
 };
