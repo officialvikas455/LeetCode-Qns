7fd9dd173fa324a5251ae47722193e21b6ea9279
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-24 13:47:43 +0530

    Time: 0 ms (100%), Space: 17.2 MB (25.89%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.cpp
@@ -3,33 +3,24 @@ public:
     int numSpecial(vector<vector<int>>& mat) {
         int m = mat.size();
         int n = mat[0].size();
-        int cnt = 0;
+
+        vector<int> row(m, 0), col(n, 0);
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (mat[i][j] == 1) {
-                    bool ok = true;
-
-                    // Check row
-                    for (int k = 0; k < n; k++) {
-                        if (k != j && mat[i][k] == 1) {
-                            ok = false;
-                            break;
-                        }
-                    }
-
-                    // Check column
-                    if (ok) {
-                        for (int k = 0; k < m; k++) {
-                            if (k != i && mat[k][j] == 1) {
-                                ok = false;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (ok) cnt++;
+                    row[i]++;
+                    col[j]++;
                 }
+            }
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && row[i] == 1 && col[j] == 1)
+                    cnt++;
             }
         }
 
