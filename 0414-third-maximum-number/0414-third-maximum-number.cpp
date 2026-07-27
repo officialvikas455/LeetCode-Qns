422325e2728cf4966892f2737975bff9eda84f90
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-27 08:51:55 +0530

    Time: 0 ms (100%), Space: 12.9 MB (47.42%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -1,17 +1,33 @@
 class Solution {
 public:
     int thirdMax(vector<int>& nums) {
-        priority_queue<int> pq;
-        set<int> s; // which store unique number
-        for (int i : nums) {
-            if (s.count(i)) continue;
-            s.insert(i);
-            pq.push(i);
+        long long first = LLONG_MIN;
+        long long second = LLONG_MIN;
+        long long third = LLONG_MIN;
+
+        for (int x : nums) {
+            // Skip duplicates
+            if (x == first || x == second || x == third) {
+                continue;
+            }
+
+            if (x > first) {
+                third = second;
+                second = first;
+                first = x;
+            } else if (x > second) {
+                third = second;
+                second = x;
+            } else if (x > third) {
+                third = x;
+            }
         }
-        int n = pq.size();
-        if (n < 3) return pq.top();
-        pq.pop();
-        pq.pop();
-        return pq.top();
+
+        // If third maximum does not exist, return the maximum
+        if (third == LLONG_MIN) {
+            return first;
+        }
+
+        return third;
     }
 };
