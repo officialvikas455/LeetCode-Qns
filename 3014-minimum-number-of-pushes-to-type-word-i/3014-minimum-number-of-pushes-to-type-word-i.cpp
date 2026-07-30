b4a772922638f51fcf4ca13b806c99e887c3f9d3
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-07-30 09:42:30 +0530

    Time: 0 ms (100%), Space: 8.5 MB (31.81%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,19 +1,43 @@
-class Solution {
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         int result =0;
+
+//         unordered_map<int,int> mp;
+
+//         int assign_key = 2;
+//         for(char &ch : word){
+//             if(assign_key > 9){
+//                 assign_key = 2;
+//             }
+//             mp[assign_key]++;
+//             result += mp[assign_key];
+//             assign_key++;
+//         }
+//         return result;
+//     }
+// };
+
+
+//Better Approach
+
+class Solution{
 public:
-    int minimumPushes(string word) {
-        int result =0;
+     int minimumPushes(string word){
+      vector<int> mp(26,0);
+      
+      for(char &ch : word){
+        mp[ch-'a']++;
+      }
+      sort(begin(mp), end(mp), greater<int>()); //dec order of freq
+      int result = 0;
 
-        unordered_map<int,int> mp;
+      for(int i=0; i< 26; i++){
+        int freq = mp[i];
+        int press = (i/8) + 1;
 
-        int assign_key = 2;
-        for(char &ch : word){
-            if(assign_key > 9){
-                assign_key = 2;
-            }
-            mp[assign_key]++;
-            result += mp[assign_key];
-            assign_key++;
-        }
-        return result;
+        result += freq * press;
+      }
+      return result;
     }
 };
