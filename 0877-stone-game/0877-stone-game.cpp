5ffa09e22b513d5224e3eefb6e981bf506cce0e0
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-02 11:29:43 +0530

    Time: 15 ms (20.49%), Space: 19.7 MB (11.06%) - LeetHub

---
+++ 0877-stone-game/0877-stone-game.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int solve(int l, int r, vector<int>& piles, vector<vector<int>>& dp) {
+        if (l == r) return piles[l];
+
+        if (dp[l][r] != -1)
+            return dp[l][r];
+
+        int a = piles[l] - solve(l + 1, r, piles, dp);
+        int b = piles[r] - solve(l, r - 1, piles, dp);
+
+        return dp[l][r] = max(a, b);
+    }
+
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+
+        return solve(0, n - 1, piles, dp) > 0;
+    }
+};
