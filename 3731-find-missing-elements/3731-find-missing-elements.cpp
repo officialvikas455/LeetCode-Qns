3e8bda1226dd587868a2d5df5e4843fd38c972f3
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-04 07:47:33 +0530

    Time: 0 ms (100%), Space: 33.6 MB (28.39%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.cpp
+++ 3731-find-missing-elements/3731-find-missing-elements.cpp
@@ -1,20 +1,43 @@
+// class Solution {
+// public:
+//     vector<int> findMissingElements(vector<int>& nums) {
+//         sort(nums.begin(), nums.end());
+//         vector<int> ans;
+//         int n = nums.size();
+
+//         int start = nums[0];
+//         int end = nums[n-1];
+
+//         unordered_map<int, int> mpp;
+//         for(int i = 0; i<n; i++){
+//             mpp[nums[i]];
+//         }
+
+//         for(int i = start; i<=end; i++){
+//             if(mpp.find(i) == mpp.end()){
+//                 ans.push_back(i);
+//             }
+//         }
+
+//         return ans;
+//     }
+// };
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     vector<int> findMissingElements(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
+        int mn = *min_element(nums.begin(), nums.end());
+        int mx = *max_element(nums.begin(), nums.end());
+
+        unordered_set<int> st(nums.begin(), nums.end());
         vector<int> ans;
-        int n = nums.size();
 
-        int start = nums[0];
-        int end = nums[n-1];
-
-        unordered_map<int, int> mpp;
-        for(int i = 0; i<n; i++){
-            mpp[nums[i]];
-        }
-
-        for(int i = start; i<=end; i++){
-            if(mpp.find(i) == mpp.end()){
+        for (int i = mn; i <= mx; i++) {
+            if (!st.count(i)) {
                 ans.push_back(i);
             }
         }
