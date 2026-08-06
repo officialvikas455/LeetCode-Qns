4a14d8435a2645d93bb81755aa7881a4bc743f86
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-06 17:27:04 +0530

    Time: 0 ms (100%), Space: 9.4 MB (54.75%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,30 +1,21 @@
+
+
 class Solution {
-public: 
-// Memoization
-//  int solve(int r, int c, int m, int n,vector<vector<int>> &dp){
-//     if(r==m-1 && c == n-1) return 1;
-//     if(r>=m || c>=n) return 0; 
-
-//     if(dp[r][c] != -1)  return dp[r][c]; 
-
-//     int right = solve(r, c+1, m, n,dp);
-//     int down = solve(r+1,c, m, n,dp);
-//     return dp[r][c] = right + down; 
-//  }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(m, vector<int>(n, 0));
+public:
+    int f(int r, int c, int m, int n, vector<vector<int>> &dp) {
+        // Base case: Reached the bottom-right corner
+        if (r == m - 1 && c == n - 1) return 1;
         
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i == 0 && j == 0) {
-                    dp[i][j] = 1;
-                } else {
-                    int up = (i > 0) ? dp[i - 1][j] : 0;
-                    int left = (j > 0) ? dp[i][j - 1] : 0;
-                    dp[i][j] = up + left;
-                }
-            }
-        }
-        return dp[m - 1][n - 1];
+        // Base case: Out of bounds
+        if (r >= m || c >= n) return 0;
+
+        if(dp[r][c] != -1) return dp[r][c];
+
+        return dp[r][c] = f(r, c + 1, m, n, dp) + f(r + 1, c, m, n, dp);
     }
-};
+
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return f(0, 0, m, n, dp);
+    }
+}; 
