b2c6f5d179179abee27d6be48d9ad9833f7e7812
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-06 08:03:28 +0530

    Time: 3 ms (2.61%), Space: 9 MB (3.33%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.cpp
@@ -1,21 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+// public:
+//     int solve(int x) {
+//         int prod = 1;
+//         while (x > 0) {
+//             prod *= (x % 10);
+//             x /= 10;
+//         }
+//         return prod;
+//     }
+
+//     int smallestNumber(int n, int t) {
+//         while (true) {
+//             if (solve(n) % t == 0)
+//                 return n;
+//             n++;
+//         }
+//     }
+// };
+
+
 
 class Solution {
 public:
-    int solve(int x) {
-        int prod = 1;
-        while (x > 0) {
-            prod *= (x % 10);
-            x /= 10;
-        }
-        return prod;
-    }
-
     int smallestNumber(int n, int t) {
         while (true) {
-            if (solve(n) % t == 0)
+            vector<int> dig;
+            int temp = n;
+
+            // store all digits
+            while (temp > 0) {
+                dig.push_back(temp % 10);
+                temp /= 10;
+            }
+
+            // product of digits
+            int prod = 1;
+            for (int i = 0; i < dig.size(); i++) {
+                prod *= dig[i];
+            }
+
+            if (prod % t == 0)
                 return n;
+
             n++;
         }
     }
