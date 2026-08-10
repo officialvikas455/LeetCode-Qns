a834fe3efa0edf997ecc484babe0ed26abc926b2
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-10 17:21:01 +0530

    Time: 43 ms (57.44%), Space: 16.7 MB (42.05%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,45 +1,22 @@
-// class Solution {
-// public:
-//     bool winnerSquareGame(int n) {
-//         vector<bool> dp(n + 1, false);
-//         for (int i = 1; i <= n; ++i) {
-//             for (int j = 1; j * j <= i; ++j) {
-//                 if (!dp[i - j * j]) {
-//                     dp[i] = true;
-//                     break; 
-//                 }
-//             }
-//         }
-        
-//         return dp[n];
-//     }
-// };
-
-
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    vector<int> dp;
+vector<int> t;
+    bool solve(int n){
+        if(n==0) 
+        return false;
 
-    bool solve(int n) {
-        if (n == 0) return false;
+        if(t[n] != -1) return t[n];
 
-        if (dp[n] != -1) return dp[n];
-
-        for (int j = 1; j * j <= n; j++) {
-            // If opponent loses after taking j*j stones
-            if (!solve(n - j * j)) {
-                return dp[n] = true;
+        for(int i=1; i*i <= n; i++){
+            if(solve(n-i*i) == false){ // for Bob looses
+                return t[n] = true; // alice jeet gyi
             }
         }
-
-        return dp[n] = false;
+        return t[n] = false;
     }
-
+   
     bool winnerSquareGame(int n) {
-        dp.assign(n + 1, -1);
-        return solve(n);
+        t.assign(n+1,-1);
+      return solve(n); // Yeh Alice ke liye h agr yeh true h to alice wins , else looses;
     }
 };
