6ec3153271efc2dd41ee4653a8735fed2b0e2cb6
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-10 17:01:50 +0530

    Time: 44 ms (47.57%), Space: 16.9 MB (39.1%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,19 +1,45 @@
+// class Solution {
+// public:
+//     bool winnerSquareGame(int n) {
+//         vector<bool> dp(n + 1, false);
+//         for (int i = 1; i <= n; ++i) {
+//             for (int j = 1; j * j <= i; ++j) {
+//                 if (!dp[i - j * j]) {
+//                     dp[i] = true;
+//                     break; 
+//                 }
+//             }
+//         }
+        
+//         return dp[n];
+//     }
+// };
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    bool winnerSquareGame(int n) {
-        // dp[i] stores whether the current player can win with i stones remaining
-        vector<bool> dp(n + 1, false);
-        
-        for (int i = 1; i <= n; ++i) {
-            for (int j = 1; j * j <= i; ++j) {
-                // If the opponent loses after we take j*j stones, we win
-                if (!dp[i - j * j]) {
-                    dp[i] = true;
-                    break; 
-                }
+    vector<int> dp;
+
+    bool solve(int n) {
+        if (n == 0) return false;
+
+        if (dp[n] != -1) return dp[n];
+
+        for (int j = 1; j * j <= n; j++) {
+            // If opponent loses after taking j*j stones
+            if (!solve(n - j * j)) {
+                return dp[n] = true;
             }
         }
-        
-        return dp[n];
+
+        return dp[n] = false;
+    }
+
+    bool winnerSquareGame(int n) {
+        dp.assign(n + 1, -1);
+        return solve(n);
     }
 };
