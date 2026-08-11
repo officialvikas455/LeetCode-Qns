17c8b542095c053244eb424048b85bf28d748265
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-11 19:40:16 +0530

    Time: 0 ms (100%), Space: 22.5 MB (77.12%) - LeetHub

--- 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
+++ 2996-smallest-missing-integer-greater-than-sequential-prefix-sum/2996-smallest-missing-integer-greater-than-sequential-prefix-sum.cpp
@@ -1,22 +1,50 @@
+// class Solution {
+// public:
+//     int missingInteger(vector<int>& nums) {
+//         int n = nums.size();
+//         int sum = nums[0];
+//         for(int i=1; i<n; i++){
+//             if(nums[i-1] + 1 == nums[i]){
+//                 sum += nums[i];
+//             }
+//             else{
+//                 break;
+//             }
+//         }
+//         unordered_set<int> st(nums.begin(), nums.end());
+
+//         while(st.count(sum)){
+//             sum++;
+//         }
+//         return sum;
+        
+//     }
+// };
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
     int missingInteger(vector<int>& nums) {
         int n = nums.size();
         int sum = nums[0];
-        for(int i=1; i<n; i++){
-            if(nums[i-1] + 1 == nums[i]){
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i - 1] + 1)
                 sum += nums[i];
-            }
-            else{
+            else
                 break;
+        }
+
+        sort(nums.begin(), nums.end());
+         int x = sum;
+        for (int num : nums) {
+            if (num == x) {
+                x++;
             }
         }
-        unordered_set<int> st(nums.begin(), nums.end());
 
-        while(st.count(sum)){
-            sum++;
-        }
-        return sum;
-        
+        return x;
     }
 };
