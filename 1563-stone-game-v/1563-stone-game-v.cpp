6f9dc34219c41982a5db708a78d95463cb1eb4cb
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-17 13:38:42 +0530

    Time: 695 ms (30.99%), Space: 16.2 MB (77.71%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.cpp
+++ 1563-stone-game-v/1563-stone-game-v.cpp
@@ -1,62 +1,44 @@
 class Solution {
 public:
-    int dp[501][501];
-
-    int solve(vector<int>& v, int l, int r, vector<int>& prefix) {
-
+    int t[501][501];
+    int solve(int l, int r, vector<int>& prefSum) {
         if (l >= r)
             return 0;
 
-        if (dp[l][r] != -1)
-            return dp[l][r];
+        if (t[l][r] != -1)
+            return t[l][r];
 
-        int ans = 0;
+        int score = 0;
 
-        for (int i = l; i < r; i++) {
-            int leftSum = prefix[i + 1] - prefix[l];
+        for (int mid = l; mid <= r - 1; mid++) {
 
-            int rightSum = prefix[r + 1] - prefix[i + 1];
+            int leftSum = prefSum[mid] - (l - 1 >= 0 ? prefSum[l - 1] : 0);
+            int rightSum = prefSum[r] - prefSum[mid];
 
             if (leftSum < rightSum) {
-
-                ans = max(ans,
-                    leftSum + solve(v, l, i, prefix)
-                );
-
-            }
-            else if (rightSum < leftSum) {
-
-                ans = max(ans,
-                    rightSum + solve(v, i + 1, r, prefix)
-                );
-
-            }
-            else {
-
-                ans = max(ans,
-                    leftSum + max(
-                        solve(v, l, i, prefix),
-                        solve(v, i + 1, r, prefix)
-                    )
-                );
+                score = max(score, leftSum + solve(l, mid, prefSum));
+            } else if (leftSum > rightSum) {
+                score = max(score, rightSum + solve(mid + 1, r, prefSum));
+            } else {
+                score = max({score, leftSum + solve(l, mid, prefSum),
+                             rightSum + solve(mid + 1, r, prefSum)});
             }
         }
-
-        return dp[l][r] = ans;
+        return t[l][r] = score;
     }
 
     int stoneGameV(vector<int>& stoneValue) {
 
         int n = stoneValue.size();
+          memset(t, -1, sizeof(t));
 
-        memset(dp, -1, sizeof(dp));
+        vector<int> prefSum(n, 0);
 
-        vector<int> prefix(n + 1, 0);
+        prefSum[0] = stoneValue[0];
 
-        for (int i = 0; i < n; i++) {
-            prefix[i + 1] = prefix[i] + stoneValue[i];
+        for (int i = 1; i < n; i++) {
+            prefSum[i] = prefSum[i - 1] + stoneValue[i];
         }
-
-        return solve(stoneValue, 0, n - 1, prefix);
+        return solve(0, n - 1, prefSum);
     }
 };
