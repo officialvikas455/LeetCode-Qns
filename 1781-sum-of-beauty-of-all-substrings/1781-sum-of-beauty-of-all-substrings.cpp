9e91439c46b2dbe5ae172c430d637f3c52af655e
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-24 19:28:58 +0530

    Time: 123 ms (33.86%), Space: 26 MB (34.31%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -3,19 +3,22 @@ public:
     int beautySum(string s) {
         
         int sum = 0;
+       
 
         for(int i=0; i<s.length(); i++){
             unordered_map<char,int> mpp;
             for(int j=i; j<s.length(); j++){
                 mpp[s[j]]++;
-                int maxi = INT_MIN;
-                int mini = INT_MAX;
+                
+                 int maxi = INT_MIN;
+                 int mini = INT_MAX;
                 for (auto it : mpp) {
                     mini = min(mini, it.second);
                     maxi = max(maxi, it.second);
                 }
                  sum += (maxi - mini);
             }
+            
            
              
         }
