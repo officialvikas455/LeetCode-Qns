14c5e6739ae71803b2da9eba9c7f2909b3f3e3ca
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-25 16:26:39 +0530

    Time: 0 ms (100%), Space: 24.2 MB (67.36%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.cpp
@@ -3,14 +3,12 @@ public:
     int missingMultiple(vector<int>& nums, int k) {
         vector<bool> present(101, false);
 
-        for (int num : nums) {
+        for(int num : nums){
             present[num] = true;
         }
 
-        for (int multiple = k; ; multiple += k) {
-            if (multiple > 100 || !present[multiple]) {
-                return multiple;
-            }
+        for(int multiple = k ; ;multiple += k){
+            if(multiple > 100 || !present[multiple]) return multiple;
         }
         return -1;
     }
