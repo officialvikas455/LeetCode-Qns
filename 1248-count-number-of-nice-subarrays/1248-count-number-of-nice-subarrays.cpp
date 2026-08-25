782ec2f3a6812be810a522b1256b9a23ca0c24ed
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-08-25 23:15:37 +0530

    Time: 75 ms (14.56%), Space: 94.3 MB (12.04%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int n = nums.size();
-        int left = 0, right = 0;
-        int oddCount = 0;
-        int totalSubarrays = 0;
-        int currentSubarrayCount = 0;
+        unordered_map<int, int> mp;
 
-        while (right < n) {
-            // If the current element is odd, increment our odd counter
-            if (nums[right] % 2 != 0) {
-                oddCount++;
-                // Reset the valid subarray count for this window
-                currentSubarrayCount = 0;
+        mp[0] = 1;
+
+        int odd = 0;
+        int cnt = 0;
+
+        for (int x : nums) {
+            if (x % 2 != 0) {
+                odd++;
             }
 
-            // When we have exactly k odd numbers, shrink the window from the left
-            while (oddCount == k) {
-                currentSubarrayCount++;
-                if (nums[left] % 2 != 0) {
-                    oddCount--;
-                }
-                left++;
+            // Need previous prefix with odd - k
+            if (mp.find(odd - k) != mp.end()) {
+                cnt += mp[odd - k];
             }
 
-            // Add the valid subarrays ending at 'right' to the total
-            totalSubarrays += currentSubarrayCount;
-            right++;
+            mp[odd]++;
         }
 
-        return totalSubarrays;
+        return cnt;
     }
 };
