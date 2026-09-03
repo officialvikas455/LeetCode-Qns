c53ba54c59d52e10ccc5a7ee3a4af6fec3bf8fee
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-03 20:53:51 +0530

    Time: 0 ms (100%), Space: 165.9 MB (31.94%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cpp
@@ -2,12 +2,15 @@ class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
         int n = nums1.size();
+        
         int minEle = *min_element(nums1.begin(), nums1.end());
-
+        
+        // If min Element is Odd then we convert all the array into Odd
         if(minEle % 2 == 1){
             return true;
         }
-
+        
+        
         for(int &num : nums1){
             if(num % 2 == 1) return false;
         }
