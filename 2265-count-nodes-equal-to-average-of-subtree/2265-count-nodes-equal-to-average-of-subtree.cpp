3cde42de2be346d19a04829ceb97ef9ccc7917e9
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-10 21:31:29 +0530

    Time: 7 ms (54.96%), Space: 15.8 MB (29.76%) - LeetHub

--- 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
+++ 2265-count-nodes-equal-to-average-of-subtree/2265-count-nodes-equal-to-average-of-subtree.cpp
@@ -1,14 +1,23 @@
 class Solution {
     int ans = 0;
-    pair<int,int> dfs(TreeNode* node) {
-        if (!node) return {0, 0};
-        auto [ls, lc] = dfs(node->left);
-        auto [rs, rc] = dfs(node->right);
-        int sum = ls + rs + node->val;
-        int cnt = lc + rc + 1;
-        if (sum / cnt == node->val) ans++;
-        return {sum, cnt};
+
+    pair<int, int> dfs(TreeNode* node) {
+        // Base case
+        if (!node)
+            return {0, 0};
+
+        auto [leftSum, leftCount] = dfs(node->left);
+
+        auto [rightSum, rightCount] = dfs(node->right);
+
+        int sum = leftSum + rightSum + node->val;
+        int count = leftCount + rightCount + 1;
+        if (sum / count == node->val)
+            ans++;
+
+        return {sum, count};
     }
+
 public:
     int averageOfSubtree(TreeNode* root) {
         dfs(root);
