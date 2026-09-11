63b634da47d5204d67b1977c8b4760a1d124a272
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-11 10:33:30 +0530

    Time: 10 ms (46.94%), Space: 30.9 MB (51.66%) - LeetHub

--- 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
+++ 3483-unique-3-digit-even-numbers/3483-unique-3-digit-even-numbers.cpp
@@ -1,35 +1,40 @@
 class Solution {
 public:
     int totalNumbers(vector<int>& digits) {
-        unordered_set<int> set;
+        unordered_set<int> st;
         int n = digits.size();
 
-        for(int i = 0; i < n; i++) {
-            if(digits[i] == 0) {
+        for (int i = 0; i < n; i++) {
+
+            // Hundreds digit cannot be 0
+            if (digits[i] == 0)
                 continue;
-            }
 
-            for(int j = 0; j < n; j++) {
-                if(j == i) {
+            for (int j = 0; j < n; j++) {
+
+                // Cannot reuse the same index
+                if (j == i)
                     continue;
-                }
 
-                for(int k = 0; k < n; k++) {
-                    if(k == i || k == j) {
+                for (int k = 0; k < n; k++) {
+
+                    // Cannot reuse i or j
+                    if (k == i || k == j)
                         continue;
-                    }
 
-                    if(digits[k] % 2 == 0) {
-                        int num = digits[i] * 100
-                                + digits[j] * 10
-                                + digits[k];
+                    // Last digit must be even
+                    if (digits[k] % 2 != 0)
+                        continue;
 
-                        set.insert(num);
-                    }
+                    int num = digits[i] * 100
+                            + digits[j] * 10
+                            + digits[k];
+
+                    st.insert(num);
                 }
             }
         }
 
-        return set.size();
+        return st.size();
     }
 };
