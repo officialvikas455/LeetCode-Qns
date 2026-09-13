eadcacbced199feefd454dc957125f8f3f135940
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-13 10:29:14 +0530

    Time: 7 ms (96.64%), Space: 13.9 MB (49.78%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     int largestOverlap(vector<vector<int>>& img1, vector<vector<int>>& img2) {
+
         int n = img1.size();
-        vector<pair<int,int>> A, B;
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (img1[i][j] == 1) A.emplace_back(i, j);
-                if (img2[i][j] == 1) B.emplace_back(i, j);
+
+        vector<pair<int, int>> pos1, pos2;
+
+        for (int i = 0; i < n; i++) { 
+            for (int j = 0; j < n; j++) {
+
+                if (img1[i][j] == 1)
+                    pos1.push_back({i, j});
+
+                if (img2[i][j] == 1)
+                    pos2.push_back({i, j});
             }
         }
-        vector<vector<int>> cnt(2 * n, vector<int>(2 * n, 0));
-        int best = 0;
-        for (auto& a : A) {
-            for (auto& b : B) {
-                int dx = b.first - a.first + n;
-                int dy = b.second - a.second + n;
-                best = max(best, ++cnt[dx][dy]);
+
+        vector<vector<int>> freq(2 * n, vector<int>(2 * n, 0));
+
+        int ans = 0;
+
+        for (auto x : pos1) {
+            for (auto y : pos2) {
+
+                int dx = y.first - x.first + n;
+                int dy = y.second - x.second + n;
+
+                freq[dx][dy]++;
+
+                ans = max(ans, freq[dx][dy]);
             }
         }
-        return best;
+
+        return ans;
     }
 };
