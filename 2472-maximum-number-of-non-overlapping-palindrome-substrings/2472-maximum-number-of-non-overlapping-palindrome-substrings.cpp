39fde629efc3d36bdb5d6f7daf8f69d6d9919e41
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-16 00:05:49 +0530

    Time: 1842 ms (6.02%), Space: 208.1 MB (18.32%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -29,10 +29,25 @@ int n;
         return t[i][j] = max({grow,slide});
      }
     int maxPalindromes(string s, int k) {
-         n = s.length();
-         if(k==1)
-            return n;
-         vector<vector<int>> t(n+1, vector<int>(n+1,-1));
-        return solve(s, k, 0, k-1,t);
+        n = s.length();
+        if(k==1) return n;
+
+        vector<vector<int>> t(n+1, vector<int>(n+1,0));
+
+        for(int i=n-1; i>=0; i--){
+            for(int j=n-1; j>=0; j--){
+                if(isPalindrome(s,i,j)){
+                    int take = 1 + (j+k <=n ? t[j+1][j+k] : 0);
+                    int grow = t[i][j+1];
+                    int slide = t[i+1][j+1];
+
+                     t[i][j] = max({take, grow, slide});
+                }
+                int grow = t[i][j+1];
+                int slide = t[i+1][j+1];
+                t[i][j] = max({t[i][j], grow, slide});
+            }
+        }
+        return t[0][k-1];
     }
 };
