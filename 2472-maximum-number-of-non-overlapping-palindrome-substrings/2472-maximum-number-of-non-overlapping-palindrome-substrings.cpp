109f5e0c2261b6c8948834066a42e054edf1836d
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-15 23:37:42 +0530

    Time: 1837 ms (6.02%), Space: 209.7 MB (14.92%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.cpp
@@ -1,44 +1,38 @@
 class Solution {
 public:
+int n;
+   bool isPalindrome(string &s, int i, int j){
+     while(i < j){
+        if(s[i] != s[j]) return false;
+        i++;
+        j--;
+     }
+     return true;
+   }
 
-    int t[2001];
-    bool isPalindrome(string &s, int i, int j) {
-        while (i < j) {
-            if (s[i] != s[j])
-                return false;
+     int solve(string &s, int k, int i, int j, vector<vector<int>> &t){
+        if(i >= n || j>=n) return 0;
 
-            i++;
-            j--;
+        if(t[i][j] != -1) {
+            return t[i][j];
+        }
+        if(isPalindrome(s,i,j)){
+            int take = 1 + solve(s,k,j+1,j+k,t);
+            int grow = solve(s,k,i,j+1,t);
+            int slide = solve(s,k,i+1,j+1,t);
+
+            return t[i][j] = max({take,grow, slide});
         }
 
-        return true;
-    }
-
-    int solve(string &s, int i, int k) {
-        int n = s.length();
-
-        if (i >= n)
-            return 0;
-
-        if (t[i] != -1)
-            return t[i];
-
-        int ans = solve(s, i + 1, k);
-
-        for (int j = i + k - 1; j < n; j++) {
-
-            if (isPalindrome(s, i, j)) {
-
-                ans = max(ans, 1 + solve(s, j + 1, k));
-            }
-        }
-
-        return t[i] = ans;
-    }
-
+        int grow = solve(s,k,i,j+1,t);
+        int slide = solve(s,k,i+1,j+1,t);
+        return t[i][j] = max({grow,slide});
+     }
     int maxPalindromes(string s, int k) {
-        memset(t, -1, sizeof(t));
-
-        return solve(s, 0, k);
+         n = s.length();
+         if(k==1)
+            return n;
+         vector<vector<int>> t(n+1, vector<int>(n+1,-1));
+        return solve(s, k, 0, k-1,t);
     }
 };
