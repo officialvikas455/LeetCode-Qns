6c775bcb36dfd57741d6837f0d4e3a449b27c91f
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-16 22:32:47 +0530

    Time: 1 ms (67.59%), Space: 15.3 MB (31.63%) - LeetHub

--- 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
+++ 1636-sort-array-by-increasing-frequency/1636-sort-array-by-increasing-frequency.cpp
@@ -1,15 +1,50 @@
 class Solution {
 public:
-    vector<int> frequencySort(vector<int>& nums) {
-        vector<int> count(201, 0);
-        for (int num : nums) {
-            count[num + 100]++;
+
+    struct Compare {
+        bool operator()(pair<int,int>& a, pair<int,int>& b) {
+
+            if(a.first == b.first) {
+                return a.second < b.second; // larger number first
+            }
+
+            return a.first > b.first; // smaller frequency first
         }
-        sort(nums.begin(), nums.end(), [&](int a, int b) {
-            if (count[a + 100] == count[b + 100])
-                return a > b;
-            return count[a + 100] < count[b + 100];
-        });
-        return nums;
+    };
+
+    vector<int> frequencySort(vector<int>& nums) {
+
+        unordered_map<int, int> mp;
+
+        for(int x : nums) {
+            mp[x]++;
+        }
+
+        priority_queue<
+            pair<int,int>,
+            vector<pair<int,int>>,
+            Compare
+        > pq;
+
+        vector<int> ans;
+
+        for(auto &y : mp) {
+            pq.push({y.second, y.first});
+        }
+
+        while(!pq.empty()) {
+
+            auto u = pq.top();
+            pq.pop();
+
+            int freq = u.first;
+            int num = u.second;
+
+            for(int i = 0; i < freq; i++) {
+                ans.push_back(num);
+            }
+        }
+
+        return ans;
     }
 };
