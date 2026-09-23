70debb66161d22365885cb7f2bf4b432e34ee9e3
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-23 09:44:35 +0530

    Time: 3 ms (71.49%), Space: 102.4 MB (38.48%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,29 +1,3 @@
-// class Solution {
-// public:
-//     int minOperations(vector<int>& nums, int x) {
-//         int n = nums.size();
-//         sort(begin(nums), end(nums));
-//         int left = 0;
-
-//         int sum = 0;
-//         int minOperations = INT_MAX;
-//         for(int right = 0; right < n; right++){
-//             sum += nums[right];
-
-//             if(sum > x){
-//                 sum -= nums[left];
-//                 left++;
-//             }
-//             else if(x - sum == 0){
-//                 return minOperations = min(minOperations, right-left+1);
-                 
-//             }
-//         }
-//         return -1;
-//     }
-// };
-
-
 
 class Solution {
 public:
@@ -57,8 +31,6 @@ public:
             }
         }
 
-        if(maxLength == -1) return -1;
-
-        return n - maxLength;
+        return maxLength == -1 ? -1 : n - maxLength;
     }
 };
