ebea20febeda418ae5ec2ad1aace6c58d6188fed
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-24 11:17:11 +0530

    Time: 6 ms (75.58%), Space: 44.6 MB (99.89%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int findContentChildren(vector<int>& g, vector<int>& s) {
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+
+        int i = 0; // child pointer
+        int j = 0; // cookie pointer
+
+        while (i < g.size() && j < s.size()) {
+            if (s[j] >= g[i]) {
+                i++; // child satisfied
+            }
+            j++; // move to next cookie
+        }
+
+        return i;
+    }
+};
