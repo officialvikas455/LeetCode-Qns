f8ed10e867228786704f9832b71b7b4f8936e11b
Vikas Gangwar <gangwarvikas6398@gmail.com>
2026-09-25 14:17:41 +0530

    Time: 0 ms (100%), Space: 7.8 MB (94.92%) - LeetHub

---
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int alternateDigitSum(int n) {
+        int sum0 = 0, sum1 = 0, count = 0;
+        while (n != 0) {
+            if (count % 2 == 0) {
+                sum0 += n % 10;
+            } else {
+                sum1 += n % 10;
+            }
+            n /= 10;
+            count++;
+        }
+        return (count % 2 == 0) ? (sum1 - sum0) : (sum0 - sum1);
+    }
+};
